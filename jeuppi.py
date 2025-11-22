5c22281210ff924667b07776c977b6775185416d
Mathias <beckersmathias@proton.me>
2025-11-23 00:14:06 +0100

    Vrai fix(c'était pas fix avant enft) + refacto fonction position

--- jeuppi.py
+++ jeuppi.py
@@ -103,24 +103,28 @@ def gerer_touche(event):
                     vaisseau_avance = True
                 elif vaisseau_avance and event.type == pygame.KEYUP:
                     vaisseau_avance = False
+            case pygame.K_e:
+                stop_vaisseau()
 
 def afficher_vaisseau(position_vaisseau, orientation_vaisseau):
-    if vaisseau_avance:
-        
+    if vaisseau_avance:  
         image_vaisseau = vaisseau_images[1]
         photo_vaisseau = pygame.transform.scale(image_vaisseau,(RAYON_VAISSEAU*2,RAYON_VAISSEAU*2))
-        photo_vaisseau_r = pygame.transform.rotate(photo_vaisseau,270+orientation_vaisseau)
+        #Il faut rajouter un moins sinon l'image du vaisseau tourne dans le mauvais sens
+        photo_vaisseau_r = pygame.transform.rotate(photo_vaisseau,-orientation_vaisseau)
         position_vaisseau_photo = pygame.Rect(position_vaisseau[0]-RAYON_VAISSEAU,position_vaisseau[1]-RAYON_VAISSEAU,2*RAYON_VAISSEAU,2*RAYON_VAISSEAU)
         fenetre.blit(photo_vaisseau_r,position_vaisseau_photo)
     else: 
         image_vaisseau = vaisseau_images[0]
         photo_vaisseau = pygame.transform.scale(image_vaisseau,(RAYON_VAISSEAU*2,RAYON_VAISSEAU*2))
-        photo_vaisseau_r = pygame.transform.rotate(photo_vaisseau,270+orientation_vaisseau)
+        #Il faut rajouter un moins sinon l'image du vaisseau tourne dans le mauvais sens
+        photo_vaisseau_r = pygame.transform.rotate(photo_vaisseau,-orientation_vaisseau)
         position_vaisseau_photo = pygame.Rect(position_vaisseau[0]-RAYON_VAISSEAU,position_vaisseau[1]-RAYON_VAISSEAU,2*RAYON_VAISSEAU,2*RAYON_VAISSEAU)
         fenetre.blit(photo_vaisseau_r,position_vaisseau_photo)       
     return 
 
-    
+# Dear Arthur, va te faire foutre avec ton code à la chat gpt ça m'a pris trois heures à débuguer
+
 # Fonction qui calcule la différence entra l'ancienne et la nouvelle position du vaisseau (afin de l'appliquer aux élément du jeu)
 def get_delta_pos(temps_maintenant,masse_vaisseau,force,orientation_vaisseau,stop=False):
     global t_avant
@@ -136,7 +140,7 @@ def get_delta_pos(temps_maintenant,masse_vaisseau,force,orientation_vaisseau,sto
         t_avant = temps_maintenant
     delta_t = temps_maintenant - t_avant
 
-    #coordonnés du vaisseau dans le repere ecran
+    #coordonnés du vaisseau dans le repere écran
     x_vaisseau = dimensions_fenetre[0]/2
     y_vaisseau = dimensions_fenetre[1]/2
     
@@ -146,32 +150,15 @@ def get_delta_pos(temps_maintenant,masse_vaisseau,force,orientation_vaisseau,sto
     ax = a*math.cos(angle_rad)
     ay = a*math.sin(angle_rad)
     
-    #calcul gravité pour chaque planete
-    for entite in scene : 
-        if entite["type"]=="planete":
-            x_planete,y_planete = entite["position"]
-
-            #distance au carré entre le vaisseau et la planete choisie
-            delta_x = x_planete-x_vaisseau
-            delta_y = y_planete-y_vaisseau
-            r2 = delta_x**2 + delta_y**2
-
-            #on applique la gravité pour un rayon appartenant à [0,rayon_influence]
-            if r2<= RAYON_INFLUENCE**2 and r2>0:
-                r = math.sqrt(r2)
-                masse_planete = entite["masse"]
-
-                #calcul gravité
-                Force_grav = CONSTANTE_GRAV*masse_planete*masse_vaisseau/r2
-                a_grav = Force_grav/masse_vaisseau
-                #on additionne la gravité à celle du moteur(+vecteur unitaire pour la direction)
-                ax+=a_grav*(delta_x/r)
-                ay+=a_grav*(delta_y/r)
+    # calcul gravité pour chaque planete
+    a_planete = calcul_gravite_planete(x_vaisseau,y_vaisseau,masse_vaisseau)
+    ax+=a_planete[0]
+    ay+=a_planete[1]
 
     #mise a jour vitesse
     vx = vx0+ax*delta_t
     vy = vy0+ay*delta_t
-
+    
     #mise a jour position du vaisseau
     x =x0 + vx0*delta_t + (ax*delta_t**2)/2
     y =y0 + vy0*delta_t + (ay*delta_t**2)/2
@@ -195,6 +182,31 @@ def get_delta_pos(temps_maintenant,masse_vaisseau,force,orientation_vaisseau,sto
 
     # On retourne la différence entre l'ancienne et nouvelle position du vaisseau
     return [x0-x,y0-y]
+
+def calcul_gravite_planete(x_vaisseau,y_vaisseau,masse_vaisseau):
+    a_planete_x = 0
+    a_planete_y = 0
+    for entite in scene : 
+            if entite["type"]=="planete":
+                x_planete,y_planete = entite["position"]
+
+                #distance au carré entre le vaisseau et la planete choisie
+                delta_x = x_planete-x_vaisseau
+                delta_y = y_planete-y_vaisseau
+                r2 = delta_x**2 + delta_y**2
+
+                #on applique la gravité pour un rayon appartenant à [0,rayon_influence]
+                if r2<= RAYON_INFLUENCE**2 and r2>0:
+                    r = math.sqrt(r2)
+                    masse_planete = entite["masse"]
+
+                    #calcul gravité
+                    Force_grav = CONSTANTE_GRAV*masse_planete*masse_vaisseau/r2
+                    a_grav = Force_grav/masse_vaisseau
+                    #on additionne la gravité à celle du moteur(+vecteur unitaire pour la direction)
+                    a_planete_x+=a_grav*(delta_x/r)
+                    a_planete_y+=a_grav*(delta_y/r)
+    return a_planete_x,a_planete_y
 
 
 def stop_vaisseau():
@@ -254,7 +266,6 @@ def afficher_planete(planete):
     fenetre.blit(photo_planete,position)
     return
 
-
 def generer_carte():
     nb_planetes=random.randint(NBR_PLANETE_MIN,NBR_PLANETE_MAX)
     couleurs = [ROUGE,JAUNE,BLEU,ORANGE,WHITE]
@@ -309,7 +320,7 @@ while True:
     delta_pos = get_delta_pos(pygame.time.get_ticks(),1,force,orientation_vaisseau)
 
     affiche(scene, delta_pos)
-    collision_planete()
+    # collision_planete()
     pygame.display.flip()
     horloge.tick(images_par_seconde)
     
